2b4be8bfc6c7d3b68a6504f613b49cb215814d20
Jhon Orozco <jhondao.desarrollo@hotmaile.com>
2025-01-15 22:12:01 -0500

    Input y manejo del estado en la aplicación

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState([ 'One Punch', 'Dragon Ball' ]);
+
+  const onAddCategory = () => {
+    setCategories(['Valorant', ...categories]);
+  }
+  
+
   return (
     <>
+        {/* titulo */}
         <h1>GifExpertApp</h1>
+        
         {/* <p>kG4LFt1YL0Jqb3xkezR3VymA0IEXvBSq </p>  */}
+
+        {/* input */}
+        <AddCategory />
+
+        {/* Lista de Gif */}
+        <button onClick={onAddCategory}>Agregar</button>
+        <ol>
+          { categories.map( categorie => {
+            return <li key={ categorie }>{categorie}</li>
+          } 
+          )}
+        </ol>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React, { StrictMode } from 'react'
 import ReactDOM,{ createRoot } from 'react-dom/client'
 import { GifExpertApp } from './GifExpertApp.jsx'
+import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
